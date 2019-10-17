fab724c721191d618744d570a202911943fcad92
Yosef Gamble <yegamble@gmail.com>
2019-10-17 13:41:10 +1300

    Added 2fa verification to notification centre
    Same issue as other controllers, I needed to add 2fa to prevent users phished passwords from granting access to user information, purchases, and accounts.

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -15,6 +15,7 @@ class NotificationController extends Controller
 
     public function __construct() {
         $this->middleware('auth');
+        $this -> middleware('verify_2fa');
     }
 
     /**
